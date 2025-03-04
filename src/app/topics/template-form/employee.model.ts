b09fa986cb40bfeff4b217a902511ba1cf45c54d
Girish J Rao <j.girish.rao@outlook.com>
2025-03-04 21:33:31 +0530

    4th March changes.

---
+++ src/app/topics/template-form/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    constructor(
+        public id:number,
+        public name: string,
+        public address: string,
+        public age: number,
+        public gender: string
+    ){
+
+    }
+}
--- src/app/topics/template-form/template-form.component.ts
+++ src/app/topics/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Component({
   selector: 'app-template-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  availableAges = [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
+  availableGender = ['Male','Female'];
+  mod = new Employee(7,'Girish J','008 Icon Luxuria',31,'Male');
+  submited : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submited = true;
   }
 
 }

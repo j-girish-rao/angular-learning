7e11de6edf988dd6bb69b60a8eff17a3a7fd0e8d
Girish J Rao <j.girish.rao@outlook.com>
2025-03-27 01:54:52 +0530

    27th March : Update existing.

--- src/app/topics/lifecycle-hooks/lifecycle-hooks.component.ts
+++ src/app/topics/lifecycle-hooks/lifecycle-hooks.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { 
+  Component, 
+  OnChanges,
+  OnInit, 
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy 
+} from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-hooks',
   templateUrl: './lifecycle-hooks.component.html',
   styleUrls: ['./lifecycle-hooks.component.scss']
 })
-export class LifecycleHooksComponent implements OnInit, OnDestroy {
+export class LifecycleHooksComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  intervalTimer : any ;
+  ngOnChanges(): void {
+    console.log('Hello from ngOnChanges()');
+  }
 
   ngOnInit(): void {
-    this.intervalTimer = setInterval(() => {
-      console.log('Hello from ngOnInit()');
-    });
+    console.log('Hello from ngOnInit()');
+  }
+
+  ngDoCheck(): void {
+    console.log('Hello from ngDoCheck()');
+  }
+  
+  ngAfterContentInit(): void {
+    console.log('Hello from ngAfterContentInit()');
+  }
+  
+  ngAfterContentChecked(): void {
+    console.log('Hello from ngAfterContentChecked()');
+  }
+  
+  ngAfterViewInit(): void {
+    console.log('Hello from ngAfterViewInit()');
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log('Hello from ngAfterViewChecked()');
   }
 
   ngOnDestroy(): void {
-    clearInterval(this.intervalTimer);
+    console.log('Hello from ngOnDestroy()');
   }
-
 }
